d394ecc5687234e654b2c96d9e810f5f612c2104
lizi <86536666+lizi2266@users.noreply.github.com>
2021-11-16 22:33:26 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,13 +4,31 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Tripop'));
 
+
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/tripop'));
+
+app.get('*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/tripop/index.html'));
+});
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/Tripop/index.html'));
-});
+    res.sendFile(path.join(__dirname+'/dist/tripop/index.html'));
+    });
+    app.get('', function(req,res) {
+    
+        res.sendFile(path.join(__dirname+'/dist/tripop/index.html'));
+        });
+        app.get('/', function(req,res) {
+    
+            res.sendFile(path.join(__dirname+'/dist/tripop/index.html'));
+            });
+// app.get('*', (req, res) => {
+//     res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
